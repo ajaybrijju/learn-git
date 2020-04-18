822ffa411a2659a7ae67bfb18ebec4195f7fb82a
ajaybrijju <ajay.brijju@gmail.com>
2020-04-17 20:23:03 -0700

    added yo

--- hungry.py
+++ hungry.py
@@ -1,9 +1,10 @@
 hungry = input("are you hungry?\n")
-if hungry=="yes":
+if hungry=="yes" and hungry=="yo":
     print("Eat samosa")
     print("Eat pizza")
     print("Eat burger")
     print("Eat fries")
+    print("added a new item")
 
 else:
     print("Do your homework! dude!!!")
