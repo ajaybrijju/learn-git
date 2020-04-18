69aef31e4beac96acc47ffd970460bb95ec19fdd
ajaybrijju <ajay.brijju@gmail.com>
2020-04-17 19:32:12 -0700

    start

--- hungry.py
+++ hungry.py
@@ -2,11 +2,7 @@ hungry = input("are you hungry?\n")
 if hungry=="yes":
     print("Eat samosa")
     print("Eat pizza")
-    print("Eat burger")
-    print("Eat fries")
-    print("added change")
 
 else:
     print("Do your homework! dude!!!")
-    print("drink water")
-    print("drink soda")
+    
